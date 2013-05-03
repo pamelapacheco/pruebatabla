e2c35509ce8029471f31683317cc23489c1ef0f6
Pamela Pacheco <pam_liz31@hotmail.com>
2013-05-03 18:24:45 -0500

    se agrega vista detalle

---
+++ pruebatabla/DetalleViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetalleViewController.h
+//  pruebatabla
+//
+//  Created by PamLiiz on 03/05/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetalleViewController : UIViewController
+
+@end
---
+++ pruebatabla/DetalleViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetalleViewController.m
+//  pruebatabla
+//
+//  Created by PamLiiz on 03/05/13.
+//
+//
+
+#import "DetalleViewController.h"
+
+@interface DetalleViewController ()
+
+@end
+
+@implementation DetalleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
